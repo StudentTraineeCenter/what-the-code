ad1440eade3e220bdd408ab55685e0e66eb4ea23
cernochmartin <cernochm@outlook.cz>
2024-04-29 12:58:54 +0200

    feat: add index and python page

--- app.vue
+++ app.vue
@@ -1,5 +1,69 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'What the Code',
+  ogTitle: 'What the Code',
+  description: 'Tato stránka je úvodní stránka mého blogu.'
+})
+</script>
+
 <template>
-  <div>
+  <TheNav />
+  <UContainer class="min-height">
     <NuxtPage />
-  </div>
+  </UContainer>
+  <TheFooter />
+
+  <UNotifications />
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+:root {
+  --green-primary: #22C55D;
+  --black: #3a3a3a;
+}
+
+* {
+  font-family: 'Poppins', sans-serif;
+}
+
+h1 {
+  font-size: 48px;
+  text-align: center;
+  color: var(--black);
+}
+
+h2 {
+  font-size: 32px;
+  color: var(--green-primary);
+  text-align: center;
+  margin-top: 32px;
+  margin-bottom: 16px;
+}
+
+footer {
+  background-color: var(--green-primary);
+  color: #ffffff;
+  padding: 12px;
+}
+
+.black-text {
+  color: var(--black);
+}
+
+.min-height {
+  min-height: 100vh;
+}
+
+@media screen and (min-width: 1280px) {
+
+  h1 {
+    font-size: 92px;
+  }
+
+  h2 {
+    font-size: 48px;
+  }
+}
+</style>
---
+++ components/the/TheNav.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const links =
+    [{
+        label: 'Domů',
+        icon: 'i-heroicons-home',
+        to: '/'
+    },
+    {
+        label: 'Python',
+        icon: 'i-heroicons-code-bracket',
+        to: '/python'
+    }]
+</script>
+
+<template>
+    <UHorizontalNavigation :links="links" class="border-b border-gray-200 dark:border-gray-800" />
+</template>
