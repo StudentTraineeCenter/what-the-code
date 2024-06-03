92a38607d084607c83192cdee764bcaa801518dd
zyt4 <michalfilip04@gmail.com>
2024-06-03 20:36:01 +0200

    Update stromek.py

--- content/python/ukoly/stromek.py
+++ content/python/ukoly/stromek.py
@@ -4,15 +4,10 @@ pocet = int(input("Zadej počet pater: "))
 # vytvoření pater
 for x in range(pocet):
     patro = "" # připravíme si proměnou pro patro(resetuje se při každém cyklu)
-    for i in range(pocet-x): # přidáme do patra odsazení
-        patro += " "
-    for i in range(x*2+1): # přidáme část stromu
-        patro += "*"
+    patro += (pocet-x)*" " # přidáme odsazení patra od strany (přidá tolikrát string " ", dá se použít místo toho, abychom dělali for cyklus)
+    patro += (x*2+1)*"*" # přidáme část stromu (přidá tolikrát string "*")
     print(patro) # vypíšeme aktuální patro stromu
 
 # vytvoření kmenu
-kmen = ""
-for x in range(pocet - 1): # vytvoříme odsazení pro kmen
-    kmen += " "
-kmen += "***"
+kmen = (pocet-1)*" " + "***"
 print(kmen) # vypíšeme kmen
